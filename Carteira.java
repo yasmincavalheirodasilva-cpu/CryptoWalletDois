abc8cb7c96de83dca51a972cc369675c1d63b49c
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-05-26 19:26:12 -0300

    feat: adiciona senha de acesso

--- Carteira.java
+++ Carteira.java
@@ -1,13 +1,15 @@
 public class Carteira {
     String dono;
     double saldoBitcoin;
+    String senhaAcesso;
 
     public Carteira(String dono) {
         this.dono = dono;
         this.saldoBitcoin = 0.0;
+        this.senhaAcesso = "1234";
     }
+
     public void depositar(double qtd) {
         this.saldoBitcoin += qtd;
-        
     }
 }
